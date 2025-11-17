a32bfdee723e072ed7672ef3753d73060cde4180
d3ad0x1 <nikki.solution@gmail.com>
2025-11-17 17:14:17 +0500

    Increase matrix background intensity

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 
 const MATRIX_CHARS = "01#@$%&*+=<>/?".split("");
-const MATRIX_COUNT = 220;
+const MATRIX_COUNT = 180;
 
 function getRandomChar() {
   return MATRIX_CHARS[Math.floor(Math.random() * MATRIX_CHARS.length)];
